cd473a163bf32e50cbef9e045f0e57db87e44d1e
WarumDach <warumdach@gmail.com>
2026-08-18 20:36:39 +0300

    tehtäviä

--- Tehtävät/Moduuli 3/3.tehtava.py
+++ Tehtävät/Moduuli 3/3.tehtava.py
@@ -3,13 +3,13 @@
 x = input("Sukupuoli M/N: ")
 y = float(input("Hemoglobiiniarvo (g/l): "))
 
-if x == "M" and y >= 117 and y <= 130:
+if x == "M" and y < 134:
     print("Hemoglobiiniarvo (g/l): alhainen")
-elif x == "M" and y >= 131 and y <= 160:
+elif x == "M" and y <= 195:
     print("Hemoglobiiniarvo (g/l): normaali")
-elif x == "N" and y >= 117 and y <= 130:
+elif x == "N" and y < 117:
     print("Hemoglobiiniarvo (g/l): alhainen")
-elif x == "N" and y >= 131 and y <= 160:
+elif x == "N" and y <= 175:
     print("Hemoglobiiniarvo (g/l): normaali")
 else:
     print("Hemoglobiiniarvo (g/l): korkea")
