9864946b1ae4de2a3652455402d2599ef64007b7
Kasperi Hurmerinta <kassuh@metropolia.fi>
2026-08-18 14:52:38 +0300

    tehtävien tekoa

--- Tehtävät/Moduuli 3/1.tehtava.py
+++ Tehtävät/Moduuli 3/1.tehtava.py
@@ -1,2 +1,9 @@
 ## 1. Tehtävä
 
+x = float(input("Anna kuhan pituus senttimetreinä: "))
+
+if x < 37:
+    kuha_pituus = 37 - a
+    print(f"Laske kuha takaisin järveen. Kuhan pitäisi olla vähintään 37cm. Teidän kuhasta puuttuu senttejä: {kuha_pituus}")
+else:
+    print("Kuha on oikean pituinen!")
